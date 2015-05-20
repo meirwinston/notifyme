2b4b94f0f013ded7abf625989b1b8408b77a42d1
meirwinston <meirwinston@yahoo.com>
2015-05-19 21:20:39 -0400

    a

---
+++ src/main/java/com/notifyme/ws/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.notifyme.ws;
+
+/**
+ * @author Meir Winston
+ */
+public class LoginResponse {
+    private String authToken;
+
+    public LoginResponse(){
+
+    }
+    public LoginResponse(String token){
+        this.authToken = token;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+}
